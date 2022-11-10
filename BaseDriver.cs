002edba9a6d06b7017434df79cad708388192a96
asesukara <karile_karile@yahoo.co.uk>
2022-11-10 22:37:02 +0200

    atnaujintas

--- BaseDriver.cs
+++ BaseDriver.cs
@@ -4,13 +4,12 @@ namespace ClassLibrary1.Page
 {
     public class BaseDriver
     {
-        protected static IWebDriver Driver;/*atirbutas*/
-        private /*kintamasis*/ const string pageUrl = "https://www.mokivezi.lt";
+        protected static IWebDriver Driver;
+        private const string pageUrl = "https://www.mokivezi.lt";
         public BaseDriver(IWebDriver webdriver)
         {
-            Driver = webdriver;/*konstuktorius*/
-        }
+            Driver = webdriver;
 
+        }
     }
 }
-
