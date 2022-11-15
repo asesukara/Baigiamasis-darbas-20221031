5fd0bc0729fa114041738388e6ee29b599dbb30d
asesukara <karile_karile@yahoo.co.uk>
2022-11-15 08:41:55 +0200

    atnaujinta

---
+++ Driver/BaseDriver.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+
+namespace ClassLibrary1.Page
+{
+    public class BaseDriver
+    {
+        protected static IWebDriver Driver;
+        private const string pageUrl = "https://www.mokivezi.lt";
+        public BaseDriver(IWebDriver webdriver)
+        {
+            Driver = webdriver;
+
+        }
+    }
+}
